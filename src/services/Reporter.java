7d7e60b5a354d809db60fe13cd52f812317e5d0f
omerfiles <omerfiles@gmail.com>
2014-10-01 11:44:45 +0300

    Added reported class

---
+++ src/services/Reporter.java
@@ -0,0 +1,45 @@
+package services;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import jsystem.framework.report.Reporter.EnumReportLevel;
+
+@Service
+public class Reporter {
+	ArrayList<String[]> reportLogs;
+
+	public Reporter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() {
+		reportLogs = new ArrayList<>();
+	}
+
+	public void report(String message){
+		//append text to list
+		reportLogs.add(new String[]{message});
+	}
+
+	public void startLevel(String string, EnumReportLevel currentplace) {
+		reportLogs.add(new String[]{string});
+
+	}
+
+	public void startLevel(String string) {
+		reportLogs.add(new String[]{string});
+
+	}
+
+	public void stopLevel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ArrayList<String[]> getReportLogs() {
+		return reportLogs;
+	}
+
+}
