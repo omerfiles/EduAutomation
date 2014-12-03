c6001000d9075d3078b7c5ee588b3925ccba39db
omerfiles <omerfiles@gmail.com>
2014-12-03 10:29:06 +0200

    Added test testDataValidationOnClassTestScoreWidget

--- src/tests/tms/dashboard/DashboardWidgetTests.java
+++ src/tests/tms/dashboard/DashboardWidgetTests.java
@@ -3,6 +3,7 @@ package tests.tms.dashboard;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
 
+import Interfaces.TestCaseParams;
 import pageObjects.EdoHomePage;
 import pageObjects.tms.DashboardPage;
 
@@ -63,51 +64,78 @@ public class DashboardWidgetTests extends BasicDashboardTest {
 		testResultService.assertEquals(courseName,
 				dashboardPage.getSelectedCourseInReport());
 	}
-	
+
 	@Test
-	public void testCompletionRateWidget()throws Exception{
+	@TestCaseParams(testCaseID = { "17097" })
+	public void testDataValidationOnClassTestScoreWidget() throws Exception {
+
+		String className = "1 student class";
+		String courseName = "2 unis";
+		String TeacherUser = "autoTeacher2";
+		String[] unitsAvgScores = new String[] { "25", "10" };
+
+		startStep("Login as the teacher and open the dashboard");
+		EdoHomePage edoHomePage = pageHelper.loginAsTeacher(TeacherUser);
+		DashboardPage dashboardPage = (DashboardPage) edoHomePage
+				.openTeachersCorner(true);
+
+		startStep("Select class and course");
+		dashboardPage.selectClassInDashBoard(className);
+		dashboardPage.selectCourseInDashboard(courseName);
+		dashboardPage.clickOnDashboardGoButton();
+
+		startStep("Check the avarage score of the units");
+		String scoreUnit1 = dashboardPage.getAvgScorePerUnitClassTestScore(0);
+		String scoreUnit2 = dashboardPage.getAvgScorePerUnitClassTestScore(1);
+		testResultService.assertEquals(unitsAvgScores[0], scoreUnit1);
+		testResultService.assertEquals(unitsAvgScores[1], scoreUnit2);
+
+	}
+
+	@Test
+	public void testCompletionRateWidget() throws Exception {
 		EdoHomePage edoHomePage = pageHelper.loginAsTeacher();
 		DashboardPage dashboardPage = (DashboardPage) edoHomePage
 				.openTeachersCorner(true);
 
 		String className = dashboardPage.getSelectedClass();
 		String courseName = dashboardPage.getSelectedCourse();
-		
+
 		startStep("Check widget title");
 		String title = dashboardPage.getWidgetTitle(2, 1);
 		testResultService.assertEquals("Completaion Rate", title,
 				"title not found");
 	}
-	
+
 	@Test
-	public void testTPSWidget()throws Exception{
+	public void testTPSWidget() throws Exception {
 		EdoHomePage edoHomePage = pageHelper.loginAsTeacher();
 		DashboardPage dashboardPage = (DashboardPage) edoHomePage
 				.openTeachersCorner(true);
 
 		String className = dashboardPage.getSelectedClass();
 		String courseName = dashboardPage.getSelectedCourse();
-		
+
 		startStep("Check widget title");
 		String title = dashboardPage.getWidgetTitle(2, 1);
 		testResultService.assertEquals("Completaion Rate", title,
 				"title not found");
 	}
-	
+
 	@Test
-	public void testPLTWidget()throws Exception{
+	public void testPLTWidget() throws Exception {
 		EdoHomePage edoHomePage = pageHelper.loginAsTeacher();
 		DashboardPage dashboardPage = (DashboardPage) edoHomePage
 				.openTeachersCorner(true);
 
 		String className = dashboardPage.getSelectedClass();
 		String courseName = dashboardPage.getSelectedCourse();
-		
+
 		startStep("Check widget title");
 		String title = dashboardPage.getWidgetTitle(3, 1);
 		testResultService.assertEquals("Placement Test", title,
 				"title not found");
-		
+
 		startStep("Check that chart is displayed");
 		dashboardPage.checkIfWidgetHasData(3, 1);
 		startStep("Click widget link button and check the report opens");
@@ -117,8 +145,8 @@ public class DashboardWidgetTests extends BasicDashboardTest {
 
 		sleep(4);
 		dashboardPage.checkForReportResults();
-//		testResultService.assertEquals(className,
-//				dashboardPage.getSelectedClassInReport());
+		// testResultService.assertEquals(className,
+		// dashboardPage.getSelectedClassInReport());
 	}
 
 }
