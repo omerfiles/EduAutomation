ae8cc583b6661d1a90cfdcb4f85fa3cadf3ef243
omerfiles <omerfiles@gmail.com>
2014-12-21 16:34:28 +0200

    Added assert on number of assigned students

--- src/tests/tms/reports/LicenseUsageReportTests.java
+++ src/tests/tms/reports/LicenseUsageReportTests.java
@@ -17,40 +17,42 @@ public class LicenseUsageReportTests extends EdusoftWebTest {
 	public void setup() throws Exception {
 		super.setup();
 	}
-	
-	
+
 	@Test
 	@TestCaseParams(testCaseID = { "16157" })
-	public void testLicenseUsageReportAsSchoolAdmin() throws Exception{
+	public void testLicenseUsageReportAsSchoolAdmin() throws Exception {
 		testLicenseUsageReportAsTMSDomain_16168(UserType.SchoolAdmin);
 	}
+
 	@Test
 	@TestCaseParams(testCaseID = { "16156" })
-	public void testLicenseUsageReportAsTeacher() throws Exception{
+	public void testLicenseUsageReportAsTeacher() throws Exception {
 		testLicenseUsageReportAsTMSDomain_16168(UserType.Teahcer);
 	}
+
 	@Test
 	@TestCaseParams(testCaseID = { "16169" })
-	public void testLicenseUsageReportAsSuperVisor() throws Exception{
+	public void testLicenseUsageReportAsSuperVisor() throws Exception {
 		testLicenseUsageReportAsTMSDomain_16168(UserType.Supervisor);
 	}
+
 	@Test
 	@TestCaseParams(testCaseID = { "16168" })
-	public void testLicenseUsageReportAsTMSadmin() throws Exception{
+	public void testLicenseUsageReportAsTMSadmin() throws Exception {
 		testLicenseUsageReportAsTMSDomain_16168(UserType.TMSAdmin);
 	}
 
 	public void testLicenseUsageReportAsTMSDomain_16168(UserType userType)
 			throws Exception {
 		String packageName = "FD-A3/6m_7m";
-		String classForTest="class1";
+		String classForTest = "class1";
 		String studentFirstName = configuration.getProperty("student");
 
 		// Login as TMSDOMAIN
 		report.startLevel("Login as " + userType.toString());
 
 		TmsHomePage tmsHomePage = null;
-		EdoHomePage edoHomePage=null;
+		EdoHomePage edoHomePage = null;
 
 		switch (userType) {
 
@@ -60,20 +62,19 @@ public class LicenseUsageReportTests extends EdusoftWebTest {
 		case Supervisor:
 			edoHomePage = pageHelper.loginAsSupervisor();
 			edoHomePage.waitForPageToLoad();
-			tmsHomePage =(TmsHomePage) edoHomePage.openTeachersCorner();
+			tmsHomePage = (TmsHomePage) edoHomePage.openTeachersCorner();
 			break;
 		case Teahcer:
 			edoHomePage = pageHelper.loginAsTeacher();
 			edoHomePage.waitForPageToLoad();
-			tmsHomePage =(TmsHomePage) edoHomePage.openTeachersCorner();
+			tmsHomePage = (TmsHomePage) edoHomePage.openTeachersCorner();
 			break;
 		case SchoolAdmin:
 			edoHomePage = pageHelper.loginAsSchoolAdmin();
 			edoHomePage.waitForPageToLoad();
-			tmsHomePage =(TmsHomePage) edoHomePage.openTeachersCorner();
+			tmsHomePage = (TmsHomePage) edoHomePage.openTeachersCorner();
 			break;
 		}
-		
 
 		// Open reports
 		report.startLevel("Click on reports");
@@ -85,28 +86,27 @@ public class LicenseUsageReportTests extends EdusoftWebTest {
 		report.startLevel("Click on license usage");
 		tmsHomePage.selectCourseReport("License Usage");
 		sleep(1);
-		if(userType==UserType.TMSAdmin){
+		if (userType == UserType.TMSAdmin) {
 			report.startLevel("Select institution");
 			tmsHomePage.selectInstitute(autoInstitution.getInstitutionName(),
 					autoInstitution.getInstitutionId(), false, false);
 			sleep(1);
 		}
-		if(userType==UserType.Supervisor){
+		if (userType == UserType.Supervisor) {
 			report.startLevel("Select teacher");
 			tmsHomePage.selectTeacher(pageHelper.getTeacher().getUserName());
 			sleep(1);
 		}
-		
+
 		report.startLevel("Select class");
-		tmsHomePage.selectClass(classForTest, false,
-				false);
+		tmsHomePage.selectClass(classForTest, false, false);
 		sleep(2);
 		report.startLevel("Select package name");
 		tmsHomePage.selectPackageByName(packageName);
 		sleep(2);
 		tmsHomePage.clickOnGo();
 		sleep(5);
-//		webDriver.printScreen();
+		// webDriver.printScreen();
 		tmsHomePage.switchToReportFrame();
 
 		ReportPage reportPage = new ReportPage(webDriver, testResultService);
@@ -116,8 +116,8 @@ public class LicenseUsageReportTests extends EdusoftWebTest {
 		testResultService.assertEquals(packageName, packageNameFromSammary,
 				"Package name not found in report sammary");
 
-		testResultService.assertEquals(classForTest,
-				classNameFromSammary, "class name not found in report sammary");
+		testResultService.assertEquals(classForTest, classNameFromSammary,
+				"class name not found in report sammary");
 
 		report.startLevel("Check for specific student details");
 		String studentFirstAndLastName = reportPage
@@ -125,6 +125,13 @@ public class LicenseUsageReportTests extends EdusoftWebTest {
 		testResultService.assertEquals(studentFirstName + " "
 				+ studentFirstName, studentFirstAndLastName,
 				"Student name not found");
+
+		report.startLevel("Check number of assigned students");
+		String studentsNumber = dbService.getNumberOfStudentsInClass(
+				classForTest, autoInstitution.getInstitutionId());
+		testResultService.assertEquals(studentsNumber,
+				reportPage.getNumberOfStudentsInClass(),
+				"Number of students incorrect or not found");
 
 	}
 }
