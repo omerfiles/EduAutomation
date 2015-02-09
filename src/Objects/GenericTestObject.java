0c9b1ded40f6c863f2e697dcdd5013b3a8f0b5c3
omerfiles <omerfiles@gmail.com>
2015-02-09 10:25:22 +0200

    added generic test object

---
+++ src/Objects/GenericTestObject.java
@@ -0,0 +1,39 @@
+package Objects;
+
+import org.junit.Before;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import services.Configuration;
+import services.DbService;
+import services.NetService;
+import services.Reporter;
+import services.TestResultService;
+import services.TextService;
+import junit.framework.TestCase;
+
+public class GenericTestObject extends TestCase {
+
+	protected Reporter report;
+//	protected Configuration configuration;
+	protected DbService dbService;
+	protected NetService netService;
+	protected TestResultService testResultService;
+	protected TextService textService;
+	
+	public ClassPathXmlApplicationContext ctx;
+
+	@Before
+	public void setup() throws Exception {
+		ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+		report = (services.Reporter) ctx.getBean("Reporter");
+//		configuration=(Configuration)ctx.getBean("configuration");
+		textService = (TextService) ctx.getBean("TextSerivce");
+		dbService = (DbService) ctx.getBean("DbService");
+		testResultService = (TestResultService) ctx
+				.getBean("testResultService");
+		
+
+	}
+
+}
