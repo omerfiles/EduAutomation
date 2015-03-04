bc775b07a3295267253c75b12c630152b8de0d3f
omerfiles <omerfiles@gmail.com>
2015-03-04 18:56:11 +0200

    updated getCourseUnits

--- src/services/DbService.java
+++ src/services/DbService.java
@@ -634,7 +634,7 @@ public class DbService extends GenericService {
 	}
 
 	public List<String> getCourseUnits(String courseId) throws Exception {
-		String sql = "select ItemId from Item where ComponentSubComponentId in (select ComponentSubComponentId from ComponentSubComponents where ComponentId in(select ComponentId from UnitComponents where UnitId in (select unitId from units where CourseId=20000)))";
+		String sql = "select ItemId from Item where ComponentSubComponentId in (select ComponentSubComponentId from ComponentSubComponents where ComponentId in(select ComponentId from UnitComponents where UnitId in (select unitId from units where CourseId="+courseId+")))";
 
 		List<String> items = getArrayListFromQuery(sql, 1);
 		return items;
